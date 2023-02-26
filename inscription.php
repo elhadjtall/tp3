6b70dbbffffcf9fd09a8a0a732e119a19ff18705
Your Name <you@example.com>
2023-02-26 13:11:12 +0100

    creation fichier inscription + modif

---
+++ inscription.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+</head>
+<body>
+    <form action="verification.php" method="POST">
+        <input type="text"      name="nom"      placeholder="nom"> <br><br>
+        <input type="text"      name="prenom"   placeholder="prenom"><br><br>
+        <input type="email"     name="email"    placeholder="email"><br> <br>
+        <input type="password"  name="password" placeholder="Password"><br><br>
+        <input type="submit"    name="submit"   value="INSCRIPTION">
+    </form>
+</body>
+</html>
